a7889b9e3196c6e5278c83738204a349ed8f00f3
Locallize <fillanlee@aol.com>
2021-05-10 01:28:49 -0400

    add pre testing

---
+++ tests/index.ts
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom";
+
+describe("first init test", () => {
+  test("test run pre commit ", () => {
+      let testRan:Boolean = true
+    expect(testRan).toBe(true);
+  });
+});
