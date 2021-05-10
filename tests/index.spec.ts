ae8e4cb51a266fcab6c1651db80975946b4b9c14
Locallize <fillanlee@aol.com>
2021-05-10 01:33:34 -0400

    resolve precommits

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom";
 
 describe("first init test", () => {
   test("test run pre commit ", () => {
-      let testRan:Boolean = true
+      const testRan = true
     expect(testRan).toBe(true);
   });
 });
