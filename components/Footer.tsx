dededd00a8bba59114dff5d927759250e33a9cff
Locallize <fillanlee@aol.com>
2021-05-25 23:58:39 -0400

    style: simple footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,11 @@
-import styled from "styled-components"
+import styled from "styled-components";
 const Footer = () => {
-    return (
-        <FooterContainer>
-            cc Minimal logic 
-        </FooterContainer>
-    )
-}
+  return <FooterContainer>&copy; 2021 MinimalSyntax</FooterContainer>;
+};
 
-export default Footer
+export default Footer;
 
 const FooterContainer = styled.footer`
-    text-align:center;
-`
+  text-align: center;
+  margin: 0 10px;
+`;
