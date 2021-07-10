0d8b2dc0f019bf5aba0ae0bd1b54ce55a8e2da70
Locallize <fillanlee@aol.com>
2021-07-09 23:25:05 -0400

    add: password validation sepcs and implimentation

--- tests/unit/signinForm.spec.ts
+++ tests/unit/signinForm.spec.ts
@@ -4,6 +4,11 @@ import {
     isEmailDomainsAllowed,
     isEmailValid,
     isNoSpace,
+    hasSpecialChar,
+    hasNumber,
+    hasUpperCase,
+    hasLowerCase,
+    getPasswordLength,
     isPasswordValid,
 } from "../../utils/userform";
 
@@ -130,5 +135,158 @@ describe("invalid emails or valid email", () => {
         const email = "janeDor@gmail.com";
         const recieve = isEmailValid(email);
         expect(recieve).toBe(true);
+    });
+});
+// password validations
+describe("validate hasSpecialChar", () => {
+    it("invalid specialChar", () => {
+        const password = "ajohnDoe";
+        const hasSpecialCharacter = hasSpecialChar(password);
+        expect(hasSpecialCharacter).toBe(false);
+    });
+    it("valid specialChar", () => {
+        const password = "$#@";
+        const hasSpecialCharacter = hasSpecialChar(password);
+        expect(hasSpecialCharacter).toBe(true);
+    });
+});
+describe("validate hasNumber", () => {
+    it("invalid hasNumber", () => {
+        const password = "ajohnDoe";
+        const recievedVal = hasNumber(password);
+        expect(recievedVal).toBe(false);
+    });
+    it("valid hasNumber", () => {
+        const password = "12345";
+        const recievedVal = hasNumber(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("valid hasNumber", () => {
+        const password = "jorn12345";
+        const recievedVal = hasNumber(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("valid hasNumber", () => {
+        const password = "SAFASfsafnasa@13==";
+        const recievedVal = hasNumber(password);
+        expect(recievedVal).toBe(true);
+    });
+});
+describe("validate hasUpperCase", () => {
+    it("valid hasUpperCase", () => {
+        const password = "ajohnDoe";
+        const recievedVal = hasUpperCase(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("valid hasUpperCase", () => {
+        const password = "JOHNDOE";
+        const recievedVal = hasUpperCase(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("valid hasUpperCase", () => {
+        const password = "JOHNDOE1";
+        const recievedVal = hasUpperCase(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("valid hasUpperCase", () => {
+        const password = "JOHNDOE1@";
+        const recievedVal = hasUpperCase(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("invalid hasUpperCase", () => {
+        const password = "johndoe";
+        const recievedVal = hasUpperCase(password);
+        expect(recievedVal).toBe(false);
+    });
+});
+describe("validate getPasswordLength", () => {
+    it("valid getPasswordLength", () => {
+        const password = "ajohoe";
+        const recievedVal = getPasswordLength(password);
+        expect(recievedVal).toBe(6);
+    });
+    it("valid getPasswordLength", () => {
+        const password = "JOHNasdDOE";
+        const recievedVal = getPasswordLength(password);
+        expect(recievedVal).toBe(10);
+    });
+    it("valid getPasswordLength", () => {
+        const password = "JOHND32OE21#!1";
+        const recievedVal = getPasswordLength(password);
+        expect(recievedVal).toBe(14);
+    });
+    it("valid getPasswordLength", () => {
+        const password = "JO2312!!vsHNDOE1@";
+        const recievedVal = getPasswordLength(password);
+        expect(recievedVal).toBe(17);
+    });
+    it("invalid getPasswordLength", () => {
+        const password = "joh2123SDnk!@n12==12oe";
+        const recievedVal = getPasswordLength(password);
+        expect(recievedVal).toBe(22);
+    });
+});
+describe("validate hasLowerCase", () => {
+    it("valid hasLowerCase", () => {
+        const password = "ajLAD@#ohnDoe";
+        const recievedVal = hasLowerCase(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("valid hasLowerCase", () => {
+        const password = "joh!@@#!nDoe";
+        const recievedVal = hasLowerCase(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("valid hasLowerCase", () => {
+        const password = "johSADASfsanDoe1";
+        const recievedVal = hasLowerCase(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("valid hasLowerCase", () => {
+        const password = "johnDoe1@";
+        const recievedVal = hasLowerCase(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("invalid hasLowerCase", () => {
+        const password = "JOHNDOE";
+        const recievedVal = hasLowerCase(password);
+        expect(recievedVal).toBe(false);
+    });
+});
+describe("validate isPasswordValid", () => {
+    it("valid isPasswordValid", () => {
+        const password = "ajohdSDF42!@sfoe";
+        const recievedVal = isPasswordValid(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("valid isPasswordValid", () => {
+        const password = "JOHNDLMDFaOE3!43%";
+        const recievedVal = isPasswordValid(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("valid isPasswordValid", () => {
+        const password = "JO9n20!=HNsafDOE1";
+        const recievedVal = isPasswordValid(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("valid isPasswordValid", () => {
+        const password = "Jpl43$OHNaskmasDOE1@";
+        const recievedVal = isPasswordValid(password);
+        expect(recievedVal).toBe(true);
+    });
+    it("invalid isPasswordValid", () => {
+        const password = "johndoe";
+        const recievedVal = isPasswordValid(password);
+        expect(recievedVal).toBe(false);
+    });
+    it("invalid isPasswordValid", () => {
+        const password = "JSDONSOMEPKF";
+        const recievedVal = isPasswordValid(password);
+        expect(recievedVal).toBe(false);
+    });
+    it("invalid isPasswordValid", () => {
+        const password = "34531242r";
+        const recievedVal = isPasswordValid(password);
+        expect(recievedVal).toBe(false);
     });
 });
--- utils/userform.ts
+++ utils/userform.ts
@@ -35,6 +35,69 @@ export const isEmailValid = (email: string): boolean => {
     if (isdotCom && isValidDomain) return true;
     return false;
 };
-export const isPasswordValid = (password: string): boolean => {
-    return true;
+export const hasSpecialChar = (password: string) => {
+    const specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    const hasSpecialChar = specialChar.test(password);
+    if (hasSpecialChar) return true;
+    return false;
 };
+export const hasNumber = (password: string) => {
+    const number = /[0-9]/;
+    const hasNumber = number.test(password);
+    if (hasNumber) return true;
+    return false;
+};
+export const hasUpperCase = (password: string) => {
+    const upperCase = /[A-Z]/;
+    const hasUpperCase = upperCase.test(password);
+    if (hasUpperCase) return true;
+    return false;
+};
+export const hasLowerCase = (password: string) => {
+    const lowerCase = /[a-z]/;
+    const hasLowerCase = lowerCase.test(password);
+    if (hasLowerCase) return true;
+    return false;
+};
+export const getPasswordLength = (password: string) => {
+    const length = password.length;
+    return length;
+};
+// create a function that checks if a password has atleast one specil character, atleast one number and atleast one upcase letter
+export const isPasswordValid = (password: string): boolean => {
+    const hasSpecialCharacter = hasSpecialChar(password);
+    const hassNumber = hasNumber(password);
+    const hasUpperCaseChar = hasUpperCase(password);
+    const hasLowerCaseChar = hasLowerCase(password);
+    const passwordLength = getPasswordLength(password);
+    if (
+        hasSpecialCharacter &&
+        hassNumber &&
+        hasUpperCaseChar &&
+        hasLowerCase &&
+        passwordLength >= 8
+    )
+        return true;
+    return false;
+    // const specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    // const number = /\d/;
+    // const upcase = /[A-Z]/;
+    // const passwordLength = password.length;
+    // const hasSpecialChar = specialChar.test(password);
+    // const hasNumber = number.test(password);
+    // const hasUpcase = upcase.test(password);
+    // if (passwordLength < 8) return false;
+    // if (!hasSpecialChar) return false;
+    // if (!hasNumber) return false;
+    // if (!hasUpcase) return false;
+    // return true;
+};
+// create test for isPasswordValid
+
+// export const isPasswordValid = (password: string): boolean => {
+//     const specialChar: RegExpMatchArray = password.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/);
+//     const upperCase: RegExpMatchArray = password.match(/[A-Z]/);
+//     const number: RegExpMatchArray = password.match(/\d/);
+//     if (specialChar && upperCase && number) return true;
+//     return false;
+// };
